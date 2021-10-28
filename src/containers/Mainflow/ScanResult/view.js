2389eb01c3d03bbfc1e1c026bc5d423981f0be70
lhjwork <jin.come.up.business@gmail.com>
2021-10-28 17:47:38 +0900

    Scanresult is modified

--- src/containers/Mainflow/ScanResult/view.js
+++ src/containers/Mainflow/ScanResult/view.js
@@ -9,52 +9,78 @@ import {
   TouchableOpacity,
   SafeAreaView,
   TouchableNativeFeedback,
+  ScrollViewBase,
+  ScrollView,
 } from 'react-native';
+import WhiteSafeAreaView from '../../../components/WhiteSafeAreaView';
 import {LongButton} from '../../../components/Botton';
 
 const view = ({navigation}) => {
   return (
-    <View style={styles.back}>
-      <View>
-        <Text>축하합니다.</Text>
-
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#FFFFFF',
-            width: 327,
-            height: 50,
-            borderRadius: 30,
-            justifyContent: 'center',
-            alignItems: 'center',
-            alignSelf: 'center',
-          }}
-          onPress={() => {
-            alert('버튼작동');
-          }}
-        >
-          <Text
-            style={{
-              textAlign: 'center',
-              fontSize: 20,
-              fontWeight: 'bold',
-              color: '#FD7F36',
-            }}
-          >
-            적립하기
-          </Text>
-          <View>
-            {' '}
-            <LongButton />
+    <WhiteSafeAreaView>
+      <View style={styles.back}>
+        <View style={{flex: 1}}>
+          <View style={styles.upperBack}>
+            <Text style={styles.h1}>축하합니다.</Text>
           </View>
-        </TouchableOpacity>
+
+          <View style={styles.point}>
+            <View style={styles.inner_point}>
+              <Text style={{alignSelf: 'center'}}>Input 기능 필요</Text>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={styles.tach}
+            onPress={() => {
+              alert('버튼작동');
+            }}>
+            <Text style={styles.btn_text}>적립하기</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.charge}></View>
+        <LongButton style={styles.go_wallet} text={'나의 지갑가기'} />
+      </ScrollView>
+    </WhiteSafeAreaView>
   );
 };
 
 export default view;
 
 const styles = StyleSheet.create({
+  upperBack: {
+    minHeight: 71,
+    backgroundColor: 'rgba(14, 15, 15, 0.8)',
+  },
+  h1: {
+    fontSize: 30,
+    color: '#ffffff',
+    alignSelf: 'center',
+    marginVertical: 25,
+  },
+  point: {
+    minHeight: 123,
+    marginHorizontal: 51,
+    marginVertical: 39,
+    borderRadius: 10,
+    backgroundColor: '#c4c4c4',
+  },
+  inner_point: {
+    borderWidth: 2,
+    borderRadius: 10,
+    minHeight: 106,
+    marginHorizontal: 11,
+    marginVertical: 8,
+    borderColor: '#ffffff',
+  },
+  charge: {
+    height: 100,
+    backgroundColor: 'black',
+    marginHorizontal: 38,
+    marginVertical: 108,
+  },
   back: {
     width: 390,
     height: 350,
@@ -62,4 +88,16 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 35,
     borderBottomRightRadius: 35,
   },
+  tach: {
+    backgroundColor: '#FFFFFF',
+    width: '85%',
+    height: 50,
+    borderRadius: 30,
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  go_wallet: {
+    marginBottom: 32,
+  },
+  btn_text: {fontSize: 20, fontWeight: 'bold', color: '#FD7F36', padding: 10},
 });
