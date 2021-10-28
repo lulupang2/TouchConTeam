b1e28e5d14e7776fa527d535e8865f7bf0e6bc23
lhjwork <jin.come.up.business@gmail.com>
2021-10-28 22:29:59 +0900

    ScanResult need to modified

--- src/containers/Mainflow/ScanResult/view.js
+++ src/containers/Mainflow/ScanResult/view.js
@@ -19,6 +19,7 @@ const view = ({navigation}) => {
   return (
     <WhiteSafeAreaView>
       <View style={styles.back}>
+        {/* <Image source={require('../../../assets/images/result_star.png')} /> */}
         <View style={{flex: 1}}>
           <View style={styles.upperBack}>
             <Text style={styles.h1}>축하합니다.</Text>
@@ -26,7 +27,10 @@ const view = ({navigation}) => {
 
           <View style={styles.point}>
             <View style={styles.inner_point}>
-              <Text style={{alignSelf: 'center'}}>Input 기능 필요</Text>
+              <View style={styles.charge_display}>
+                <Text style={styles.charge_pay}>500</Text>
+                <Text style={styles.right_text}>Touch</Text>
+              </View>
             </View>
           </View>
           <TouchableOpacity
@@ -40,7 +44,13 @@ const view = ({navigation}) => {
       </View>
 
       <ScrollView style={{flex: 1}}>
-        <View style={styles.charge}></View>
+        <View style={styles.charge}>
+          <View style={styles.display}>
+            <Text style={styles.charge_text}> 적립액</Text>
+            <Text style={styles.charge_text}>2500</Text>
+            <Text style={styles.right_text}>Touch</Text>
+          </View>
+        </View>
         <LongButton style={styles.go_wallet} text={'나의 지갑가기'} />
       </ScrollView>
     </WhiteSafeAreaView>
@@ -67,6 +77,18 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: '#c4c4c4',
   },
+  charge_display: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 48,
+    marginVertical: 45,
+  },
+  charge_pay: {
+    fontSize: 30,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
   inner_point: {
     borderWidth: 2,
     borderRadius: 10,
@@ -77,9 +99,19 @@ const styles = StyleSheet.create({
   },
   charge: {
     height: 100,
-    backgroundColor: 'black',
+    backgroundColor: 'rgba(14, 15, 15, 0.8)',
     marginHorizontal: 38,
-    marginVertical: 108,
+    marginVertical: 70,
+  },
+  charge_text: {color: '#ffffff', fontSize: 20, fontWeight: 'bold'},
+  right_text: {color: '#ffffff'},
+  display: {
+    marginHorizontal: 20,
+    marginVertical: 40,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   back: {
     width: 390,
