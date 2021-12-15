844cb0d7d8eddd05e7f57d99ff9d98fed342afe8
100dongwoo <qorehddn123@naver.com>
2021-12-15 14:11:09 +0900

    remove err

--- src/containers/Mainflow/Intro2/view.js
+++ src/containers/Mainflow/Intro2/view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, Image, ImageBackground} from 'react-native';
+import {StyleSheet, Text, Image, ImageBackground, View} from 'react-native';
 import TouchableNoFeedback from '../../../components/TouchableNoFeedback';
 
 export default function Intro2({navigation}) {
@@ -13,20 +13,38 @@ export default function Intro2({navigation}) {
           source={require('../../../assets/images/intro3_h2.png')}
           style={styles.h2}
         />
-        <Text style={styles.content}>
-          터치콘 브랜드 파트너에서 보내는 리워드콘을 획득하세요!
-        </Text>
+        <View style={{flexDirection: 'row', padding: 30}}>
+          <Text style={styles.content}>
+            {
+              '터치콘 브랜드 파트너에서 보내는 리워드콘을 획득하세요! 최소 10 코인부터, 최대 30만 코인까지, 랜덤 잭팟이 기다립니다. 10 코인이상 당첨 확률 무려 100%!!! 리워드콘에 있는 QR코드를 확인하세요.\n\n그리고 터치콘앱으로 스캔하세요.리워드콘은\n“꽝”이 없습니다. 꾸준히 적립만 해도 목돈이 됩니다!!!'
+            }
+          </Text>
 
-        <Text style={styles.content}>
-          {
-            '\n최소 10 코인부터, 최대 30만 코인까지, 랜덤 잭팟이 기다립니다. 10 코인이상 당첨 확률 무려 100%!!!'
-          }
-        </Text>
-        <Text style={styles.content}>
-          {
-            '리워드콘에 있는 QR코드를 확인하세요.\n\n그리고 터치콘앱으로 스캔하세요.리워드콘은\n“꽝”이 없습니다. 꾸준히 적립만 해도 목돈이 됩니다!!!'
-          }
-        </Text>
+          {/*<Text style={styles.content}>*/}
+          {/*  {*/}
+          {/*    '\n최소 10 코인부터, 최대 30만 코인까지, 랜덤 잭팟이 기다립니다. 10 코인이상 당첨 확률 무려 100%!!!'*/}
+          {/*  }*/}
+          {/*</Text>*/}
+          {/*<Text style={styles.content}>*/}
+          {/*  {*/}
+          {/*    '리워드콘에 있는 QR코드를 확인하세요.\n\n그리고 터치콘앱으로 스캔하세요.리워드콘은\n“꽝”이 없습니다. 꾸준히 적립만 해도 목돈이 됩니다!!!'*/}
+          {/*  }*/}
+          {/*</Text>*/}
+          {/*<Text style={styles.content}>*/}
+          {/*  터치콘 브랜드 파트너에서 보내는 리워드콘을 획득하세요!*/}
+          {/*</Text>*/}
+
+          {/*<Text style={styles.content}>*/}
+          {/*  {*/}
+          {/*    '\n최소 10 코인부터, 최대 30만 코인까지, 랜덤 잭팟이 기다립니다. 10 코인이상 당첨 확률 무려 100%!!!'*/}
+          {/*  }*/}
+          {/*</Text>*/}
+          {/*<Text style={styles.content}>*/}
+          {/*  {*/}
+          {/*    '리워드콘에 있는 QR코드를 확인하세요.\n\n그리고 터치콘앱으로 스캔하세요.리워드콘은\n“꽝”이 없습니다. 꾸준히 적립만 해도 목돈이 됩니다!!!'*/}
+          {/*  }*/}
+          {/*</Text>*/}
+        </View>
         <Image
           source={require('../../../assets/images/intro3_bottom.png')}
           style={{
@@ -49,11 +67,14 @@ const styles = StyleSheet.create({
     position: 'relative',
     top: -20,
   },
+
   content: {
+    flex: 1,
+    flexWrap: 'wrap',
     color: '#FFFFFF',
-    fontSize: 14,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    width: '85%',
+    // fontSize: 14,
+    // fontWeight: 'bold',
+    // alignSelf: 'center',
+    // width: '85%',
   },
 });
