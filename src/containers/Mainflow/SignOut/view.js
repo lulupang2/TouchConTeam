081ed6ae30feed92dfff1687fc465f7bc92d48ce
100dongwoo <qorehddn123@naver.com>
2022-01-10 13:11:23 +0900

    color test

--- src/containers/Mainflow/SignOut/view.js
+++ src/containers/Mainflow/SignOut/view.js
@@ -203,7 +203,7 @@ const view = () => {
                 justifyContent: 'center',
                 marginVertical: 11,
               }}>
-              <Text style={{color: '#의', fontSize: 15, fontWeight: '700'}}>
+              <Text style={{color: '#FF0000', fontSize: 15, fontWeight: '700'}}>
                 *아래 주의사항 꼭 읽어주세요!'
               </Text>
             </RowView>
@@ -214,49 +214,70 @@ const view = () => {
           style={{
             marginHorizontal: 25,
             color: '#FF0000',
-            fontSize: 15,
-            fontWeight: '700',
+            fontSize: 12,
+            fontWeight: 'bold',
             marginVertical: 22,
           }}>
           (주의) 터치콘앱에서 탈퇴하시면 모든 디지털 자산이 소멸됩니다.
         </Text>
 
-        <Image
-          source={require('../../../assets/images/signoutContent.png')}
-          resizeMode="contain"
-          style={{marginHorizontal: 24, maxWidth: 326, maxHeight: 364}}
-        />
+        {/*<Image*/}
+        {/*  source={require('../../../assets/images/signoutContent.png')}*/}
+        {/*  resizeMode="contain"*/}
+        {/*  style={{marginHorizontal: 24, maxWidth: 326, maxHeight: 364}}*/}
+        {/*/>*/}
+        <View style={{paddingHorizontal: 24}}>
+          <Text style={styles.Information}>
+            터치콘앱에서 탈퇴하시면 향후 터치콘 월렛 및 계정에 접근 및 사용이
+            불가능합니다. 모든 터치콘 및 다른 디지털 자산에 대한 모든 권한이
+            소멸되고 복구되지 않습니다.{'\n\n'}만약 탈퇴하신다면 이미 획득한
+            모든 디지털 자산에 대한 전송 및 처분을 하신 후에 탈퇴하시길
+            바랍니다.
+            {'\n\n'}계정 탈퇴시에는 터치콘에서 시행한 이벤트 및 프로모션에서
+            획득한 모든 리워드의 권한 및 이력이 사라지며, 이후로는 전혀 복구되지
+            않으니 주의하시기 바랍니다.{'\n\n'}터치콘앱은 계정 탈퇴 이후에는
+            모든 정보 내역이 소멸되므로 복구되지 않습니다. 브랜드 파트너의 정보
+            및 개인정보도 모두 일괄 폐기됩니다. {'\n\n'}계정을 탈퇴하시면
+            개인정보처리방침에 따라 의무적 보관을 제외한 모든 회원의 정보는 즉시
+            삭제됩니다.
+          </Text>
+          <Text style={styles.RedInfor}>
+            {'\n\n'}
+            본인은 위의 모든 사항에 대해 동의하며 향후 터치콘앱 운영진에게 그
+            어떠한 이의도 제기하지 않을 것을 약속드립니다.
+          </Text>
+        </View>
 
-        {/* <NormalLabel
-          style={{paddingVertical: 30, paddingHorizontal: 23}}
-          text={
-            '1. 서비스를 탈퇴하면 월렛에 보유한 모든 터치 토\n' +
-            '    큰의 권한이 소멸되어 복구가 되지 않습니다.\n' +
-            '\n' +
-            '2. 서비스를 탈퇴하면 스테이킹에 참여한 터치토큰\n' +
-            '    을 비롯한 모든 권한 및 정보가 소멸되어 복구가\n' +
-            '    불가능합니다.\n' +
-            '\n' +
-            '3. 탈퇴를 인한 모든 정보는 재가입을 해도 복구되\n' +
-            '    지 않습니다. 만약 탈퇴를 하실 경우에는 사전에\n' +
-            '    모든 자산에 대한 사용,전송 등을 하신 후에 탈\n' +
-            '    퇴하시기를 권장합니다.\n' +
-            '\n' +
-            '4. 본인의 탈퇴로 인해 모든 권한과 서비스는 종료\n' +
-            '    및 소멸되어니 이 점 참고하시어 신중히 결정하\n' +
-            '    시기 바랍니다.'
-          }
-        /> */}
+        {/*<NormalLabel*/}
+        {/*  style={{paddingVertical: 30, paddingHorizontal: 23}}*/}
+        {/*  text={*/}
+        {/*    '1. 서비스를 탈퇴하면 월렛에 보유한 모든 터치 토\n' +*/}
+        {/*    '    큰의 권한이 소멸되어 복구가 되지 않습니다.\n' +*/}
+        {/*    '\n' +*/}
+        {/*    '2. 서비스를 탈퇴하면 스테이킹에 참여한 터치토큰\n' +*/}
+        {/*    '    을 비롯한 모든 권한 및 정보가 소멸되어 복구가\n' +*/}
+        {/*    '    불가능합니다.\n' +*/}
+        {/*    '\n' +*/}
+        {/*    '3. 탈퇴를 인한 모든 정보는 재가입을 해도 복구되\n' +*/}
+        {/*    '    지 않습니다. 만약 탈퇴를 하실 경우에는 사전에\n' +*/}
+        {/*    '    모든 자산에 대한 사용,전송 등을 하신 후에 탈\n' +*/}
+        {/*    '    퇴하시기를 권장합니다.\n' +*/}
+        {/*    '\n' +*/}
+        {/*    '4. 본인의 탈퇴로 인해 모든 권한과 서비스는 종료\n' +*/}
+        {/*    '    및 소멸되어니 이 점 참고하시어 신중히 결정하\n' +*/}
+        {/*    '    시기 바랍니다.'*/}
+        {/*  }*/}
+        {/*/>*/}
 
         <RowView
           style={{
             borderWidth: 1,
             minHeight: 40,
             minWidth: 230,
-            marginLeft: 21,
-            marginRight: 51,
+            marginHorizontal: 25,
             borderColor: '#c4c4c4',
             padding: 11,
+            marginTop: 25,
           }}>
           <Text
             style={{
@@ -319,6 +340,20 @@ const view = () => {
 };
 
 const styles = StyleSheet.create({
+  RedInfor: {
+    fontSize: 15,
+    lineHeight: 17,
+    color: '#FF0000',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+  },
+  Information: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 12,
+    lineHeight: 15,
+    color: '#000000',
+  },
   SubTilte: {
     color: '#000000',
     fontSize: 20,
