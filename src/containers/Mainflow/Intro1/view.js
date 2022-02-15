0a6cfb403f279af99af4692af6d8e7681eff19d5
lhjwork <jin.come.up.business@gmail.com>
2022-02-15 13:47:49 +0900

    intro 1 수정

--- src/containers/Mainflow/Intro1/view.js
+++ src/containers/Mainflow/Intro1/view.js
@@ -6,6 +6,7 @@ import {
   ImageBackground,
   Alert,
   Dimensions,
+  View,
   SafeAreaView,
 } from 'react-native';
 import api from '../../../api';
@@ -15,64 +16,64 @@ const {height} = Dimensions.get('window');
 
 export default function Intro1({navigation}) {
   return (
-    <SafeAreaView>
-      <TouchableNoFeedback onPress={() => navigation.navigate('Intro2')}>
-        <ImageBackground
-          source={require('../../../assets/images/intro2_back.png')}
-          resizeMode="cover"
-          style={{width: '100%', height: '100%'}}>
+    <SafeAreaView style={{backgroundColor: '#123169', flex: 1}}>
+      <TouchableNoFeedback
+        onPress={() => navigation.navigate('Intro2')}
+        style={{flex: 1}}>
+        <View style={{flex: 1}}>
           <Image
             source={require('../../../assets/images/intro2_h2.png')}
             style={styles.h2}
           />
-          <Text
-            style={{
-              color: '#FFFFFF',
-              width: '100%',
-              height: 225,
-              fontSize: 36,
-              fontWeight: 'bold',
-              marginLeft: 47,
-              position: 'relative',
-            }}>
+          <Text style={styles.MainText}>
             {'터치콘에서\n랜덤 보상의 세계를 \n만나보세요!'}
           </Text>
-          <Text
-            style={{
-              color: '#FFFFFF',
-              width: '100%',
-              fontSize: 14,
-              fontWeight: 'bold',
-              position: 'absolute',
-              zIndex: 3,
-              top: 300,
-              right: -50,
-            }}>
+          <Text style={styles.subText}>
             글로벌 최초 디지털 자산 랜덤 리워드 - 터치콘
           </Text>
+        </View>
 
-          <Image
-            source={require('../../../assets/images/intro2_cat.png')}
-            style={styles.cat}
-          />
-        </ImageBackground>
+        <Image
+          source={require('../../../assets/images/intro2_cat.png')}
+          style={styles.cat}
+        />
       </TouchableNoFeedback>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  cat: {
-    resizeMode: 'contain',
+  subText: {
+    backgroundColor: '#123169',
+    color: '#FFFFFF',
     width: '100%',
-    height: height * 0.555,
-    alignSelf: 'center',
+    fontSize: 14,
+    fontWeight: 'bold',
+    position: 'absolute',
+    zIndex: 3,
+    top: 300,
+    right: -50,
+  },
+  MainText: {
+    backgroundColor: '#123169',
+    color: '#FFFFFF',
+    width: '100%',
+
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginLeft: 47,
+    position: 'relative',
+  },
+  cat: {
+    resizeMode: 'stretch',
+    width: '100%',
+    height: height * 0.6,
+    bottom: 0,
   },
   h2: {
     resizeMode: 'contain',
     width: '80%',
     alignSelf: 'center',
     position: 'relative',
-    top: -2,
   },
 });
